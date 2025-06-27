b5a425f0d6c14856af5c9760a05c5797ea0bb72c
Simon Zeulner <you@example.com>
2025-06-27 23:06:38 +0200

    nmap dashboad with ui

--- Website.py
+++ Website.py
@@ -1,40 +1,24 @@
-from flask import Flask, render_template_string
-import subprocess
-import re
+from flask import Flask, render_template
 from nmap import PortScanner
-from nmap import PortScannerHostDict
 
 app = Flask(__name__)
 
-HTML_TEMPLATE = """
-<!DOCTYPE html>
-<html>
-<head><title>Command Output</title></head>
-<body>
-  <h1>Command Output</h1>
-  <pre>{{ output }}</pre>
-</body>
-</html>
-"""
-
-def get_module(ip, hostname, port):
-    return f"I found {hostname} at {ip}:{port}"
-
 @app.route("/")
 def run_command():
-    # Replace this with any command you want
     scanner = PortScanner()
     scanner.scan("192.168.2.115/24", ports="80,8000", arguments="--open")
-    website = []
+    modules = []
     for ip in scanner.all_hosts():
         host = scanner[ip]
         hostname = host.hostname()
         for port in host.all_tcp():
-            module = get_module(ip=ip, port=port, hostname=hostname)
+            data = {
+                "address": f"{ip}:{port}",
+                "host": hostname
+            }
+            modules.append(data)
 
-            website.append(module)
-
-    return render_template_string(HTML_TEMPLATE, output="\n".join(website))
+    return render_template("index.html", modules=modules)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
